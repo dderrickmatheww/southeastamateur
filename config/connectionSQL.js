1f7429b21f449975a9467e5bc1ae06da96d3dd9b
Derrick <dderrickmatheww@gmail.com>
2019-05-20 09:34:10 -0400

    update on mysql

--- config/connectionSQL.js
+++ config/connectionSQL.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     // Your port; if not 3306
     port: 3306,
     // Your username
